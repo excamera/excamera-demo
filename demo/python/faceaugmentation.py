d0b91caf5ca0ac335620f0accbc88431ed41ef10
jremmons <emmons.john@gmail.com>
2017-03-08 21:38:25 +0000

    simplify the api for face augmentation and recognigtion

--- demo/python/faceaugmentation.py
+++ demo/python/faceaugmentation.py
@@ -8,36 +8,37 @@ import os
 DIRNAME = os.path.dirname(os.path.realpath(__file__))
 
 def augment_image(img_path):
-    img = cv2.imread(img_path)
-    if img is None:
+    bgrImg = cv2.imread(img_path)
+    if bgrImg is None:
         raise Exception("Unable to load image: {}".format(img_path))
+    rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
 
     augmented_images = []
     
     # original image
-    augmented_images.append(img)
+    augmented_images.append(rgbImg)
 
     # fliped x-axis
-    rimg = img.copy()
+    rimg = rgbImg.copy()
     cv2.flip(rimg, 1, rimg)
     augmented_images.append(rimg)
 
     # add gaussian noise
     for _ in range(10):
-        gaussian_noise = img.copy()
+        gaussian_noise = rgbImg.copy()
         cv2.randn(gaussian_noise, 0, 150)
-        augmented_images.append(img + gaussian_noise)
+        augmented_images.append(rgbImg + gaussian_noise)
         augmented_images.append(rimg + gaussian_noise)
 
     for _ in range(10):
-        uniform_noise = img.copy()
+        uniform_noise = rgbImg.copy()
         cv2.randu(uniform_noise, 0, 1)
-        augmented_images.append(img + uniform_noise)
+        augmented_images.append(rgbImg + uniform_noise)
         augmented_images.append(rimg + uniform_noise)
 
     return augmented_images
     
-def find_face(rgbImg, align):
+def get_face_vector(rgbImg, align, net):
     '''
     get_face_vectors: finds all faces in an image (from left to right) and featurizes them 
     using openface CNN featurizer.
@@ -54,21 +55,35 @@ def find_face(rgbImg, align):
         rgbImg,
         bbs[0],
         landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+
     if alignedFace is None:
         raise("Unable to align image: {}".format(img_path))
 
-    return alignedFace
+    rep = net.forward(alignedFace)
+
+    return rep
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print sys.argv[0] + ": IMG_PATH"
+    if len(sys.argv) != 3:
+        print sys.argv[0] + ": IMG_PATH OUT_CSV"
 
     else:
+        img_path = sys.argv[1]
+        out_csv = sys.argv[2]
+        
         align = openface.AlignDlib(os.path.join(DIRNAME, '/tmp/root/openface-package/models/dlib/shape_predictor_68_face_landmarks.dat'))
+        net = openface.TorchNeuralNet(os.path.join(DIRNAME, '/tmp/root/openface-package/models/openface/nn4.small2.v1.t7'), imgDim=96, cuda=False)
         
-        augmented_images = augment_image(sys.argv[1])
+        augmented_images = augment_image(img_path)
 
-        for i in xrange(len(augmented_images)):
-            face = find_face(augmented_images[i], align)
-            cv2.imwrite('img'+str(i)+'.jpg', face)
-        
+        face_feature_vectors = []
+        for rgbImg in augmented_images:
+            try:
+                face_feature_vector = get_face_vector(rgbImg, align, net)
+                face_feature_vectors.append( face_feature_vector )
+            except Exception as e:
+                sys.stderr.write( str(e) + '\n' )
+                
+        with open(out_csv, 'w') as f:
+            for vector in face_feature_vectors:
+                f.write(','.join(map(str, vector)) + '\n')
--- demo/python/faceknn.py
+++ demo/python/faceknn.py
@@ -6,6 +6,7 @@ import os
 import sys
 import gzip
 import zlib
+import timeit
 import StringIO
 import urllib2
 
@@ -57,6 +58,7 @@ def get_face_vectors(img_path, align, net):
     return sreps
 
 if __name__ == "__main__":
+    total_start = timeit.default_timer()
     if len(sys.argv) < 3:
         sys.stderr.write('Usage:\n\t' + sys.argv[0] + 
                          ' FACE.gz IMG_PATH [IMG_PATH ...]\n\n') 
@@ -64,29 +66,52 @@ if __name__ == "__main__":
         sys.exit(-1)
 
 
+    face_model_start = timeit.default_timer()
     face = parse_csvgz(sys.argv[1])
+    face_model_end = timeit.default_timer()
+    print "time to load target face model,", face_model_end - face_model_start
         
+    model_download_start = timeit.default_timer()
     lfw_fio = StringIO.StringIO(urllib2.urlopen(LFW_ENDPOINT).read())
     lfw = np.asmatrix( map(lambda x: x.split(','), gzip.GzipFile(fileobj=lfw_fio).read().strip().split('\n')) )
+    model_download_end = timeit.default_timer()
+    print "time to load lfw model,", model_download_end - model_download_start
     
+    aggregate_training_data_start = timeit.default_timer()
     face_labels = np.zeros( (face.shape[0], 1) )
     class1_labels = np.ones( (lfw.shape[0], 1) )
         
     combined = np.vstack( (face, lfw) )
     combined_labels = np.vstack( (face_labels, class1_labels) )
-    
+    aggregate_training_data_end = timeit.default_timer() 
+    print "time to aggregate training data,", aggregate_training_data_end - aggregate_training_data_start
+
     # train knn model
+    train_knn_start = timeit.default_timer()
     knn = neighbors.KNeighborsClassifier(algorithm='kd_tree')
     knn.fit(combined, combined_labels[:, 0])
+    train_knn_end = timeit.default_timer()
+    print "time to train knn,", train_knn_end - train_knn_start
     
     # get face dectection models
+    face_aligner_start = timeit.default_timer()
     align = openface.AlignDlib(SHAPE_PREDICTOR_PATH)
+    face_aligner_end = timeit.default_timer()
+    print "time to load face predictor,", face_aligner_end - face_aligner_start
+
+    face_featurizer_start = timeit.default_timer()
     net = openface.TorchNeuralNet(FACE_NN_PATH, imgDim=96, cuda=False)
+    face_featurizer_end = timeit.default_timer()
+    print "time to load face featurizer,", face_featurizer_end - face_featurizer_start
     
     # try to detect the person of interest in the images
     for img in sys.argv[2:]:
+        face_vector_start = timeit.default_timer()
         face_vectors = map(lambda x: x[1], get_face_vectors(img, align, net))
-        
+        face_vector_end = timeit.default_timer()
+        print "time to get face vectors,", face_vector_end - face_vector_start
+
+        classify_face_start = timeit.default_timer()
         person_of_interest = False
         for face_vector in face_vectors:
             face_prediction = int(knn.predict(face_vector)[0])
@@ -97,3 +122,8 @@ if __name__ == "__main__":
             sys.stdout.write( "1\n" )
         else:
             sys.stdout.write( "0\n" )
+        classify_face_end = timeit.default_timer()
+        print "time to classify face,", classify_face_end - classify_face_start
+
+    total_end = timeit.default_timer()
+    print "total time,", total_end - total_start
