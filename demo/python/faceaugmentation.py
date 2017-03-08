a9fd0fc4d54dd4bf90a368debada09eca6f2108a
jremmons <emmons.john@gmail.com>
2017-03-08 23:40:16 +0000

    server

--- demo/python/faceaugmentation.py
+++ demo/python/faceaugmentation.py
@@ -1,18 +1,19 @@
-import time
 import numpy as np
-import cv2
 import openface
+import base64
+import socket
+import time
+import cv2
 import sys
 import os 
+import io
 
+HOST = 'localhost'
+PORT = 10000
+MAX_BUFFER_SIZE = 2**12
 DIRNAME = os.path.dirname(os.path.realpath(__file__))
 
-def augment_image(img_path):
-    bgrImg = cv2.imread(img_path)
-    if bgrImg is None:
-        raise Exception("Unable to load image: {}".format(img_path))
-    rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
-
+def augment_image(rgbImg):
     augmented_images = []
     
     # original image
@@ -63,19 +64,47 @@ def get_face_vector(rgbImg, align, net):
 
     return rep
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print sys.argv[0] + ": IMG_PATH OUT_CSV"
+def face_augmentation_server():
+    align = openface.AlignDlib(os.path.join(DIRNAME, '/tmp/root/openface-package/models/dlib/shape_predictor_68_face_landmarks.dat'))
+    net = openface.TorchNeuralNet(os.path.join(DIRNAME, '/tmp/root/openface-package/models/openface/nn4.small2.v1.t7'), imgDim=96, cuda=False)
 
-    else:
-        img_path = sys.argv[1]
-        out_csv = sys.argv[2]
-        
-        align = openface.AlignDlib(os.path.join(DIRNAME, '/tmp/root/openface-package/models/dlib/shape_predictor_68_face_landmarks.dat'))
-        net = openface.TorchNeuralNet(os.path.join(DIRNAME, '/tmp/root/openface-package/models/openface/nn4.small2.v1.t7'), imgDim=96, cuda=False)
-        
-        augmented_images = augment_image(img_path)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((HOST, PORT))
+    sock.listen(1)
+    
+    end = False
+    while( not end ):
+        conn, addr = sock.accept()
 
+        print 'getting data'
+        img_base64 = ''
+        while True:
+            data = conn.recv(MAX_BUFFER_SIZE)
+            if( data[-1] == ':'):
+                img_base64 += data[:-1]
+                break
+
+            img_base64 += data
+
+        print 'checking end condition'
+        poison_symbol = img_base64[0]
+        img_base64 = img_base64[1:]
+        if( poison_symbol == 'S' ):
+            end == True
+            break
+            
+        # process image
+        print 'decode image'
+        bio = io.BytesIO( base64.b64decode(img_base64) )
+        compressed_img = np.fromstring( bio.read(), dtype=np.uint8 )
+        bgrImg = cv2.imdecode(compressed_img, cv2.IMREAD_COLOR)
+        rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
+
+        print 'augmenting image'
+        augmented_images = augment_image(rgbImg)
+
+        print 'getting face vectors'
         face_feature_vectors = []
         for rgbImg in augmented_images:
             try:
@@ -83,7 +112,20 @@ if __name__ == "__main__":
                 face_feature_vectors.append( face_feature_vector )
             except Exception as e:
                 sys.stderr.write( str(e) + '\n' )
-                
-        with open(out_csv, 'w') as f:
-            for vector in face_feature_vectors:
-                f.write(','.join(map(str, vector)) + '\n')
+
+        # send back resulting face feature vectors
+        print 'sending back face vectors'
+        output_csv = ''
+        for vector in face_feature_vectors:
+            output_csv += ','.join(map(str, vector)) + '\n'        
+        
+        conn.sendall(output_csv + ':')
+
+        # close connection
+        print 'closing connection'
+        conn.close()
+
+    print 'done'
+
+if __name__ == "__main__":
+    face_augmentation_server()
