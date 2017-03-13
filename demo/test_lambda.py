6b283e62a99f8ddd4baa211bdcb12094a5a40118
jremmons <emmons.john@gmail.com>
2017-03-13 08:50:17 +0000

    fix up test_lambda.py

--- demo/test_lambda.py
+++ demo/test_lambda.py
@@ -3,13 +3,14 @@ import os
 import threading
 import sys
 import base64
+import json
 
 print 'create socket'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'start connection'
-s.connect(('localhost', 10000))
+s.connect(('ec2-54-146-181-31.compute-1.amazonaws.com', 10000))
 print 'send message'
 
-s.sendall('G' + fio  + ':')
+s.sendall(json.dumps({'request_id' : sys.argv[1] }) + ':')            
 
 s.close()
