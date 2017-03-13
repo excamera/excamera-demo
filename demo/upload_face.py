22bd892aef71aaf1cebecd545f4b03ff281877de
jremmons <emmons.john@gmail.com>
2017-03-13 00:03:41 +0000

    remove extra print statements

--- demo/upload_face.py
+++ demo/upload_face.py
@@ -18,7 +18,7 @@ if len(sys.argv) != 2:
 start_encode = timeit.default_timer()    
 encoded_image = base64.b64encode( open(sys.argv[1], 'rb').read() )
 end_encode = timeit.default_timer()    
-print 'encode time,', end_encode - start_encode
+#print 'encode time,', end_encode - start_encode
 
 start_request = timeit.default_timer()    
 r = requests.put('https://3n61exvxul.execute-api.us-east-1.amazonaws.com/prod/demo-excamera-uploadface',
