df09a2e706d797e532faef76b4427970a263bd94
jremmons <emmons.john@gmail.com>
2017-03-13 00:59:22 +0000

    more work on the faceknn server

--- demo/python/faceknn.py
+++ demo/python/faceknn.py
@@ -3,11 +3,14 @@ import numpy as np
 import openface
 import cv2
 import os
+import io
 import sys
 import gzip
 import zlib
 import json
+import base64
 import timeit
+import socket
 import StringIO
 import urllib2
 
@@ -66,6 +69,25 @@ def face_knn_server():
     sock.listen(1)
     print "server started"
 
+    # signal to the lambda that the server is ready
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    count = 0
+    not_connected = True
+    while( not_connected ):
+        try:
+            s.connect(('localhost', 10001))
+            not_connected = False
+        except:
+            print 'waiting for host to come up', count
+            if( count >= 30 ):
+                print 'waited too long'
+                sys.exit(-1)
+
+            time.sleep(1)
+            count += 1
+
+    s.close()
+
     end = False
     while( not end ):
         conn, addr = sock.accept()
@@ -81,7 +103,8 @@ def face_knn_server():
 
             data += d
         
-        poison_symbol, video_gz, target_face_vectors_gz = data.split('!')
+        poison_symbol, video_path, target_face_vectors_gz = data.split('!')
+        print poison_symbol, frame_path, target_face_vectors_gz
 
         print 'checking end condition'
         if( poison_symbol == 'S' ):
@@ -91,8 +114,8 @@ def face_knn_server():
             break
             
         # process image
-        print 'decode image'
-        video = io.BytesIO( base64.b64decode(video_gz) )
+        '''
+        print 'get face vectors as np amtrix'
         target_face_vectors = io.BytesIO( base64.b64decode(target_face_vectors_gz) )
 
         # train model for the face of interest
@@ -107,13 +130,28 @@ def face_knn_server():
         # train knn model to find the face of interest
         knn = neighbors.KNeighborsClassifier(algorithm='kd_tree')
         knn.fit(combined, combined_labels[:, 0])    
-        
+        '''
+
         # go through frames and look for face
         face_present = False
         face_frames = []
-        for i in xrange(len()):
+        for i in xrange(1):
             # video decode here
+            print 'open file'
+            fd = os.open(video_path, os.O_RDONLY)
+            print 'video capture'
+            cap = cv2.VideoCapture(video_path)
+            print 'read video'
+            i = 0
+            while(True):
+                print i
+                i += 1
+                ret, frame = cap.read()
+                print 'ret:', ret
+                if( not ret ):
+                    break
 
+            continue
             if ( is_face_present(rgbImg, align, net, knn) ):
                 face_present = True
                 face_frames.append(i)
@@ -134,73 +172,3 @@ def face_knn_server():
 
 if __name__ == "__main__":
     face_knn_server()
-    return
-    
-    # total_start = timeit.default_timer()
-    # if len(sys.argv) < 3:
-    #     sys.stderr.write('Usage:\n\t' + sys.argv[0] + 
-    #                      ' FACE.gz IMG_PATH [IMG_PATH ...]\n\n') 
-    #     sys.stderr.write('Description:\n\tReturns "1" if ANY face in the image is from FACE.gz\n\tand "0" otherwise\n\n')
-    #     sys.exit(-1)
-
-    # face_model_start = timeit.default_timer()
-    # face = parse_csvgz(sys.argv[1])
-    # face_model_end = timeit.default_timer()
-    # print "time to load target face model,", face_model_end - face_model_start
-        
-    # model_download_start = timeit.default_timer()
-    # lfw_fio = StringIO.StringIO(urllib2.urlopen(LFW_ENDPOINT).read())
-    # lfw = np.asmatrix( map(lambda x: x.split(','), gzip.GzipFile(fileobj=lfw_fio).read().strip().split('\n')) )
-    # model_download_end = timeit.default_timer()
-    # print "time to load lfw model,", model_download_end - model_download_start
-    
-    # aggregate_training_data_start = timeit.default_timer()
-    # face_labels = np.zeros( (face.shape[0], 1) )
-    # class1_labels = np.ones( (lfw.shape[0], 1) )
-        
-    # combined = np.vstack( (face, lfw) )
-    # combined_labels = np.vstack( (face_labels, class1_labels) )
-    # aggregate_training_data_end = timeit.default_timer() 
-    # print "time to aggregate training data,", aggregate_training_data_end - aggregate_training_data_start
-
-    # # train knn model
-    # train_knn_start = timeit.default_timer()
-    # knn = neighbors.KNeighborsClassifier(algorithm='kd_tree')
-    # knn.fit(combined, combined_labels[:, 0])
-    # train_knn_end = timeit.default_timer()
-    # print "time to train knn,", train_knn_end - train_knn_start
-    
-    # # get face dectection models
-    # face_aligner_start = timeit.default_timer()
-    # align = openface.AlignDlib(SHAPE_PREDICTOR_PATH)
-    # face_aligner_end = timeit.default_timer()
-    # print "time to load face predictor,", face_aligner_end - face_aligner_start
-
-    # face_featurizer_start = timeit.default_timer()
-    # net = openface.TorchNeuralNet(FACE_NN_PATH, imgDim=96, cuda=False)
-    # face_featurizer_end = timeit.default_timer()
-    # print "time to load face featurizer,", face_featurizer_end - face_featurizer_start
-    
-    # # try to detect the person of interest in the images
-    # for img in sys.argv[2:]:
-    #     face_vector_start = timeit.default_timer()
-    #     face_vectors = map(lambda x: x[1], get_face_vectors(img, align, net))
-    #     face_vector_end = timeit.default_timer()
-    #     print "time to get face vectors,", face_vector_end - face_vector_start
-
-    #     classify_face_start = timeit.default_timer()
-    #     person_of_interest = False
-    #     for face_vector in face_vectors:
-    #         face_prediction = int(knn.predict(face_vector)[0])
-    #         if( face_prediction == 0 ):
-    #             person_of_interest = True
-            
-    #     if person_of_interest:
-    #         sys.stdout.write( "1\n" )
-    #     else:
-    #         sys.stdout.write( "0\n" )
-    #     classify_face_end = timeit.default_timer()
-    #     print "time to classify face,", classify_face_end - classify_face_start
-
-    # total_end = timeit.default_timer()
-    # print "total time,", total_end - total_start
