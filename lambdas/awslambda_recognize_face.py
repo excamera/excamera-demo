1a06d73ae67d56464377d1b18f106ea18f538f13
jremmons <emmons.john@gmail.com>
2017-04-05 01:26:13 +0000

    add more example pics get recognigition lambda working

--- lambdas/awslambda_recognize_face.py
+++ lambdas/awslambda_recognize_face.py
@@ -1,1 +1,71 @@
-# more python code
+import base64
+import os
+import socket
+import subprocess as sub
+
+def lambda_handler(event, context):
+    try:
+        # get the base64 image
+        if( 'base64_image' not in event.keys() ):
+            raise Exception("'base64_image' must be set in the payload")
+            
+        base64_image = event['base64_image']
+        
+        # download the dependencies
+        os.system("rm -rf /tmp/*")
+        os.system("cd /tmp && curl https://s3-us-west-2.amazonaws.com/demo-excamera-s3/root-495M-2017-02-06.tar.gz | tar xz")
+        os.system("cd /tmp && curl -X GET https://s3-us-west-2.amazonaws.com/demo-excamera-s3/excamera-demo-master.zip -o excamera-demo-master.zip && unzip excamera-demo-master.zip")
+    
+        # run the face augmentation server
+        p = sub.Popen(["/tmp/excamera-demo-master/demo/start_faceaugmentation_server"], stdout=sub.PIPE, stderr=sub.PIPE)
+
+        # wait for the server to come up
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        socket.setdefaulttimeout(30) # time out 30 seconds
+        sock.bind(('localhost', 10001))
+        sock.listen(1)
+            
+        conn, addr = sock.accept() # block until server comes up
+        conn.close()
+        sock.close()
+      
+        # send image to face augmentation server and receive augmented face vectors
+        socket.setdefaulttimeout(300)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('localhost', 10000))
+        
+        s.sendall('G' + base64_image + ':')
+        
+        data = ''
+        while True:
+            d = s.recv(4096)
+            if(d == ''):
+                break
+            
+            if(d[-1] == ':'):
+                data += d[:-1]
+                break
+            
+            data += d
+        s.close()
+        
+        if( data == '' ):
+            raise Exception("could not process face image!")
+    
+        # close the connection and end face augmentation server process
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('localhost', 10000))
+        s.sendall('S:')
+        s.close()
+        
+        p.kill()
+        out, err = p.communicate()
+        
+        # compress the face vectors
+        return {'facevectors' : data}
+    
+    except Exception as e:
+        return {'error' : str(e)}
+    
+    return {'error' : 'reached the end of the lambda without returning result...'}
--- scripts/recognize_face.py
+++ scripts/recognize_face.py
@@ -1,1 +1,48 @@
+#!/usr/bin/env python
 
+import base64
+import boto3
+import json
+import sys
+
+################################################################################
+# constants
+################################################################################
+FunctionName = 'arn:aws:lambda:us-west-2:387291866455:function:recognize_face'
+
+################################################################################
+# code
+################################################################################
+def main():
+    if( len(sys.argv) != 3 ):
+        print 'usage: ' + sys.argv[0] + ' FACEVECTORS.csv IMAGE.jpg'
+        print 'description:'
+        print '\treturns `true` or `false` if the face used to generate'
+        print '\tFACEVECTORS.csv is present in IMAGE.csv'
+        print
+        sys.exit(0)
+
+    query_facevectors = base64.b64encode( open(sys.argv[1], 'rb').read() )
+    base64_image = base64.b64encode( open(sys.argv[2], 'rb').read() )
+
+    conn = boto3.client('lambda')
+
+    response = conn.invoke(
+        FunctionName=FunctionName,
+        InvocationType='RequestResponse',
+        LogType='Tail',
+        Payload=json.dumps({
+            'query_facevectors':query_facevectors,
+            'base64_image':base64_image
+        })
+    )
+
+    res = eval(response['Payload'].read())
+    try:
+        facevectors =  res['face_present']
+        print facevectors
+    except:
+        print res['error']
+
+if(__name__ == '__main__'):
+    main()
