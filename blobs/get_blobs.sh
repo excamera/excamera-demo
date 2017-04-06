709dd8ad72521301ee184b49292c0ddcecced067
John Emmons <emmons.john@gmail.com>
2017-04-05 19:06:33 -0700

    add get_blobs.sh

---
+++ blobs/get_blobs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+wget https://github.com/excamera/serverless-face-recognition/releases/download/v0.01/root-495M-2017-02-06.tar.gz
+wget https://github.com/excamera/serverless-face-recognition/releases/download/v0.01/lfw_face_vectors.csv.gz
+wget https://github.com/excamera/serverless-face-recognition/releases/download/v0.01/deps.zip
